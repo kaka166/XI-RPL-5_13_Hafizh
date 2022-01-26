48978126031c5fa9c7f4302523cf89e224f417e0
kaka166 <hafizhfaruq858@gmail.com>
2022-01-26 07:52:44 +0700

    Praktek2

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,22 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/about', function (){
-    return view('about');
+    return view('about', [
+        "title" => "About",
+        "nama" => "Hafizh Dwi Andhika Faruq",
+        "email" => "3103120100@student.smktelkom-pwt.sch.id",
+        "gambar" => "senyuman.jpg"
+    ]);
 });
 
 Route::get('/gallery', function (){
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
 
 Route::get('/', function (){
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
